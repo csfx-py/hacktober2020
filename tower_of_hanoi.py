34069da42fac182b9bba5c202363b52c1f90f4ef
Abdul Pitodia <43836746+Abdul-pitodia@users.noreply.github.com>
2020-10-01 19:48:07 +0530

    Create tower_of_hanoi.py

---
+++ tower_of_hanoi.py
@@ -0,0 +1,21 @@
+# Tower of hanoi
+
+
+def solve(N,s,d,h):
+  global cnt
+  cnt += 1
+  #print(cnt)
+  if N == 1:
+    print(f'Moving plate {N} from {s} to {d}')
+    return
+  
+  solve(N-1,s,h,d)
+  print(f'Moving plate {N} from {s} to {d}')
+  solve(N-1,h,d,s)
+  return cnt
+
+
+
+N = int(input())
+s,h,d,cnt = 1,2,3,0
+print(solve(N,s,d,h))
