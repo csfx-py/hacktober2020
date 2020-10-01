91f36aa78602666768879e614b6475a855034e81
Rohit Jangid <iamrohitjangid21@gmail.com>
2020-10-01 16:16:10 +0530

    Some changes

--- sorting/mergeSort.cxx
+++ sorting/mergeSort.cxx
@@ -11,7 +11,7 @@ using namespace std ;
 void mergeSort(vector<int> &arr, int low ,int high) ;
 void mergeArray(vector<int> &arr,int low , int mid, int high) ;
 
-int cost = 0 ;
+
 
 int main(int argc, char **argv) {
 	vector<int> arr = {9,8,7,6,5,4,3,2,1,-1,-2,-3,-4,-6} ;
@@ -22,14 +22,12 @@ int main(int argc, char **argv) {
 	cout << "\n" ;
 	for(int i : arr)
 		cout << i << " " ;
-	cout << "\n" << cost ;
 	
 	return 0 ;
 }
 
 void mergeSort(vector<int> &arr, int low ,int high) {
 	if( low<high ){
-		cost++ ;
 		int mid = low + (high-low)/2 ;
 		mergeSort(arr,low,mid) ;
 		mergeSort(arr,mid+1,high) ;
@@ -44,19 +42,16 @@ void mergeArray(vector<int> &arr,int low , int mid, int high) {
 	int arr1[n1] ,arr2[n2] ;
 	
 	while(i<n1) {
-		cost++ ;
 		arr1[i] = arr[low + i ] ;
 		i++ ;
 	}
 	while(j<n2){
-		cost++ ;
 		arr2[j] = arr[mid + j + 1] ;
 		j++ ;
 	}
 	i = 0 ,j = 0 ;
 	k = low ;
 	while ( i < n1 && j < n2 ){
-		cost++ ;
 		if(arr1[i] <= arr2[j])
 			arr[k++] = arr1[i++] ;
 		
