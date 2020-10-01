27e5860eb5009b2e765ac59b6bf856fff6981e45
Vishwas Badiger <47709960+agasthya36@users.noreply.github.com>
2020-10-01 16:22:25 +0530

    Create fibonacci.c
    Explain Child and parent process by making child process execute fibonacci program

---
+++ fibonacci.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+#include<unistd.h>
+ #include<sys/types.h>
+ void childprocess(unsigned long long int k)
+ {
+ 	int v;
+ 	int a=0;
+ 	int b=1;
+ 	for (int j=0;j<k;j++){
+ 		if (j<=1){
+ 			v=j;
+ 		}
+ 		else{
+ 			v=a+b;
+ 			a=b;
+ 			b=v;
+ 		}
+ 		printf("%d ",v);
+ 	}
+ 	printf("\n");
+ }
+
+ 	
+ void main(void)
+ {
+ 	pid_t pid;
+ 	unsigned long long int i;
+ 	scanf("%lld",&i);
+ 	pid=fork();
+ 	//t=getpid();
+ 	if (pid==0){
+ 		printf("Child process is calling Fibonacci\n");
+ 		childprocess(i);
+ 		printf("Child process has ended\n");
+ 	}
+ 	else{
+ 		printf("This is parent process\n");
+ 		printf("Parent process has ended\n");
+ 	}
+ }
+ 	
