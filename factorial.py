15181c3ac60869369e49b1d0eda723556cdc84e0
gauravsinha18 <59147380+gauravsinha18@users.noreply.github.com>
2020-10-04 21:03:50 +0530

    accept

--- factorial.py
+++ factorial.py
@@ -1,6 +1,6 @@
-#Adithya Pai
-n=int(input("Enter a Number "))
-fact = 1 
+#gs
+n=int(input("Enter the Number "))
+f = 1 
 for i in range(1,n+1):
-   fact=fact*i
-print("Factorial is ", fact)
+   f=f*i
+print("Factorial is ", f)
