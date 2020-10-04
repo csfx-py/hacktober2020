154bf2664d04196c4012f186dd6e18e7cd48c5b9
sourav-nanda <67404111+sourav-nanda@users.noreply.github.com>
2020-10-04 22:42:36 +0530

    Create Among Us Imposter probability calculator.py

---
+++ Among Us Imposter probability calculator.py
@@ -0,0 +1,7 @@
+#This problem is available here https://edabit.com/challenge/b36bBpsnzyDbd4mzF
+
+def imposter_formula(i, p):
+    #i-Imposter(s)
+    #p-Player(s)
+    return str(round(100*(i/p)))+'%'
+		
